1cb51ef9eec358f7b9558f52f7ded0ca2b042172
Michael Alegre <malegre@litespeedtech.com>
2023-04-28 13:56:56 -0400

    Refactor class Search code.
    Refactored class Search code to inline the use of intermediate variables
    where able, replace $visitor uses as an array with equivalent uses as an
    object, simplify logic where able, add a PHP docblock @noinspection line,
    remove leading '\' character from use line, and add a new use line.
    
    upload/src/addons/LiteSpeedCache/XF/Pub/Controller/Search.php
    [Refactor] checkCsrfifNeeded(): Inlined use of intermediate variable
    $visitor.
    Replaced $visitor uses as an array with equivalent uses as an object.
    Swapped user_id check to be an early return and moved it's existing
    logic to occur after this check.
    Added PHP docblock @noinspection line for 'PhpUndefinedClassInspection'.
    [Refactor] Removed leading '\' character from use line.
    [Refactor] Added new use line for 'XF'.

--- upload/src/addons/LiteSpeedCache/XF/Pub/Controller/Search.php
+++ upload/src/addons/LiteSpeedCache/XF/Pub/Controller/Search.php
@@ -7,19 +7,20 @@
 
 namespace LiteSpeedCache\XF\Pub\Controller;
 
-use \XF\Mvc\ParameterBag;
+use XF;
+use XF\Mvc\ParameterBag;
 
 class Search extends XFCP_Search
 {
 
     public function checkCsrfIfNeeded( $action, ParameterBag $params )
     {
-        $visitor = \XF::visitor();
-
-        if ( $visitor['user_id'] && $visitor['user_id'] != 0 ) {
-            parent::checkCsrfIfNeeded($action, $params);
+        if ( XF::visitor()->user_id == 0 ) {
+            return;
         }
-        // else bypass check
+
+        /** @noinspection PhpUndefinedClassInspection */
+        parent::checkCsrfIfNeeded($action, $params);
     }
 
 }
