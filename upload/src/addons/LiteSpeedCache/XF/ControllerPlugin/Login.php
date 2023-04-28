d81c74a652be6d5adeac56b1ef48a16c6e5be778
Michael Alegre <malegre@litespeedtech.com>
2023-04-28 12:27:44 -0400

    Refactored class Login code.
    Refactored class Login to add new use lines, add new @noinspection
    lines, and make formatting/spacing improvements.
    
    upload/src/addons/LiteSpeedCache/XF/ControllerPlugin/Login.php
    [Refactor] Added new use lines for 'XF' and 'XF\Entity\User'.
    [Refactor] completeLogin(): Added @noinspection lines for 'PhpUnused'
    and 'PhpUndefinedClassInspection'.
    [Refactor] Made spacing/formatting improvements.

--- upload/src/addons/LiteSpeedCache/XF/ControllerPlugin/Login.php
+++ upload/src/addons/LiteSpeedCache/XF/ControllerPlugin/Login.php
@@ -8,21 +8,29 @@
 namespace LiteSpeedCache\XF\ControllerPlugin;
 
 use LiteSpeedCache\Listener as LscListener;
+use XF;
+use XF\Entity\User;
 
 class Login extends XFCP_Login
 {
-    public function completeLogin( \XF\Entity\User $user, $remember )
+    /**
+     *
+     * @noinspection PhpUnused
+     */
+    public function completeLogin( User $user, $remember )
     {
-        if ( $user->user_id !== \XF::visitor()->user_id ) {
+        if ( $user->user_id !== XF::visitor()->user_id ) {
             /**
              * Set custom cookie to better track logged in state when
              * "Stay logged in" is unchecked.
              */
-            \XF::app()->response()->
-                    setCookie(LscListener::LOGGED_IN_COOKIE_NAME, 1);
+            XF::app()->response()
+            ->setCookie(LscListener::LOGGED_IN_COOKIE_NAME, 1)
+            ;
         }
 
-       parent::completeLogin($user, $remember);
+        /** @noinspection PhpUndefinedClassInspection */
+        parent::completeLogin($user, $remember);
     }
 
 }
